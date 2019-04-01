91b1a0afb1590ef8b30bcd93ab16c47d4334e52a
a0938 <a0938@DESKTOP-NGCU2VI>
2019-04-02 00:09:35 +0800

    unit4

---
+++ unit04_as_students/src/java/uibeans/CarSelectionBean.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uibeans;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import repository.Car;
+import repository.CarRepositoryBean;
+
+/**
+ *
+ * @author hychen39@gm.cyut.edu.tw
+ */
+@Named(value = "carSelectionBean")
+@SessionScoped
+public class CarSelectionBean implements Serializable{
+
+    
+    // Inject the car repository
+    // DO NOT REMOVE THE CODE
+    @EJB CarRepositoryBean repository;
+   
+    /**
+     * Action method to redirect to displayInfo facelets page.
+     * @return target page name
+     */
+    public String dispalyInfo(){
+        return "displayInfo?faces-redirect=true";
+    }
+    
+    public List<Car> getCars(){
+        return repository.findAll();
+    }
+    
+    
+}
